e4e9d8d3b80e63eed2e8e6ff08130c7c25e79632
zh200578 <zh200578@qq.com>
2024-08-20 04:07:34 +0800

    Add files via upload

--- deyi.py
+++ deyi.py
@@ -2,94 +2,80 @@ import tkinter
 import sqlite3
 import tkinter as tk
 from tkinter import messagebox
-
-win = tkinter.Tk()
-win.geometry("400x400")
-conn = sqlite3.connect("ZHANG.db")
-cur = conn.cursor()
-#
+from datetime import datetime
 
 
-def on_key_press(event):
+class CashbookApp:
+    def __init__(self, root):
 
-    return "break"
-def cunru():
-    data = text_1.get("1.0", tk.END)
+        self.root = root
+        self.root.title = "现金账簿"
+        self.conn = sqlite3.connect("ZHANG.db")
+        self.cur = self.conn.cursor()
+        self.conn.commit()
 
-    cur.execute("INSERT INTO cashbook(data) VALUES(?)", (data,))
-    conn.commit()
-    text_1.delete("1.0", tk.END)
-    tk.messagebox.showinfo("保存成功", "数据已成功保存到数据库！")
+        # 保存键文本与按钮
+        self.text_1 = tkinter.Text(self.root, height=2, width=15)
+        self.text_1.pack(pady=15)
+        b1 = tkinter.Button(self.root, text="保存至数据库", command=self.cunru, width="20")
+        b1.pack(padx=20)
 
-def cha():
-    cur.execute("SELECT data FROM cashbook")
-    conn.commit()
-    rows=cur.fetchall()
-    text_2.delete("1.0",tk.END)
-    for row in rows:
-        text_2.insert(tk.END, row[0])
+        # 查询键文本与按钮
+        self.text_2 = tkinter.Text(self.root, height=2, width=15, wrap=tk.WORD)
+        self.text_2.bind("<Control-v>", self.on_paste)
+        self.text_2.bind("<Key>", self.on_key_press)
+        self.text_2.pack(pady=10)
+        b2 = tkinter.Button(self.root, text="查询数据", command=self.cha, width=20)
+        b2.pack(pady=15, padx=15)
+        # 删除键文本和按钮
+        b3 = tkinter.Button(self.root, text="删除数据", command=self.delete, width=10)
+        b3.pack()
 
+    def date(self):
+        now = datetime.now()
+        fa_now = now.strftime("%Y-%m-%d")
+        self.cur.execute("INSERT INTO cashbook(data) VALUES(?)", (fa_now,))
+        self.conn.commit()
+        return fa_now
 
+    def cunru(self):
+        text_store = self.text_1.get("1.0", tk.END)
+        text_xiu = text_store.strip()
+        if not text_xiu:
+            tkinter.messagebox.showwarning("警告！", "存入内容不能为空！请重新输入")
+            self.text_1.delete("1.0", tk.END)
+        else:
+            self.text_1.delete("1.0", tk.END)
+            messagebox.showinfo("提示", "已保存至数据库")
+            self.cur.execute("INSERT INTO cashbook(ru) VALUES(?)", (text_store,))
+        self.conn.commit()
 
+    def cha(self):
+        self.cur.execute("SELECT ru  FROM cashbook")
+        self.conn.commit()
+        rows = self.cur.fetchall()
+        self.text_2.delete("1.0", tk.END)
+        for row in rows:
+            self.text_2.insert(tk.END, str(row[0]) + '\n')
+        if not rows:
+            messagebox.showwarning("提示", "未找到相关数据！请更换查找条件")
+        else:
+            messagebox.showwarning("提示", "数据已查询完毕！")
 
-text_1 = tkinter.Text(win, height=10, width=10)
-text_1.pack(pady=10)
-b1 = tkinter.Button(win, text="保存至数据库", command=cunru, width="20")
-b1.pack()
-text_2 = tkinter.Text(win,height=10,width=10)
-text_2.bind("<Key>", on_key_press)
-text_2.pack(pady=20)
-b2 = tkinter.Button(win,text="查询数据",command=cha,width=20)
-b2.pack()
-win.mainloop()
+    def delete(self):
+        self.cur.execute("DELETE FROM cashbook")
+        messagebox.showwarning("提示", "数据已全部删除！")
 
+    def on_paste(self, event):
+        messagebox.showinfo("提示", "粘贴操作已被禁用。")
+        return "break"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#cur.execute('''CREATE TABLE cashbook
-#             (data text UNIQUE,
-#             chu int,
-#             ru int,
+    def on_key_press(self, event):
+        return "break"
 
 
+root = tk.Tk()
+root.geometry("800x600")
+app = CashbookApp(root)
 
-
-
-
-
-
-
-
-
-
-
-#             yu int)''')
+root.mainloop()
